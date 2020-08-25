d95f1fa10e7d63ad444a49c1ebd37d678e27b40a
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-08-25 20:31:02 +0200

    final commit!

---
+++ public/precache-manifest.acd6652561b562fc051c4710b1d25616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c823827e25fc1ba14a83c1fdadd026d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9ce84fb1cd4ee80198",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0a12133a2a6ed0535c4f",
+    "url": "/static/js/2.5a2dc47e.chunk.js"
+  },
+  {
+    "revision": "dba6c7cbf3e2c690119a165676f203c3",
+    "url": "/static/js/2.5a2dc47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9ce84fb1cd4ee80198",
+    "url": "/static/js/main.dbe3a8db.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
